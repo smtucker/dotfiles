c5002e31b48c17df22b1c1caf7ec604e8142a5f2
Shelby Tucker <shelby.tucker@gmail.com>
2023-12-06 07:31:47 -0500

    nvim: Change directory telescope keymap

--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -4,10 +4,16 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<C-t>"] = {"<cmd> Telescope cder <CR>", "Telescope Change Directory"},
   },
   v = {
     [">"] = { ">gv", "indent"},
+  },
+}
+
+M.telescope = {
+  n ={
+    ["<C-t>"] = {"<cmd> Telescope cder <CR>", "Telescope Change Directory"},
+    ["<leader>fk"] = {"<cmd> Telescope keymaps <CR>", "Telescope keymaps"},
   },
 }
 
