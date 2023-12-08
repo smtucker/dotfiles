65c9ef419e222854951562ea4b1d2735f6ec9fe6
Shelby Tucker <shelby.tucker@gmail.com>
2023-12-07 21:51:12 -0500

    tmux: Fixed errors in bash tpm install script

--- install/tmux.sh
+++ install/tmux.sh
@@ -1,10 +1,11 @@
-#!/ ebin/bash
+#!/bin/bash
 
 # Check for existing folders to make sure we make them before stowing
 # If the symbolic links are there we will download to dotfiles
 
-if ! pkgmng.sh tmux; then
+if ! ./pkgmng.sh tmux; then
   exit 1
+fi
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Press C-a+I to launch the plugin manager" 
