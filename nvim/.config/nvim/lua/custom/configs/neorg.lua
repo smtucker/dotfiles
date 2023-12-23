3a6732a6b18cb1391e128fbd452a2b01585936e8
Shelby Tucker <shelby.tucker@gmail.com>
2023-12-23 14:51:17 -0500

    nvim: neorg todo undone icon a blank space

--- nvim/.config/nvim/lua/custom/configs/neorg.lua
+++ nvim/.config/nvim/lua/custom/configs/neorg.lua
@@ -1,7 +1,17 @@
 require("neorg").setup {
   load = {
     ["core.defaults"] = {}, -- Loads default behaviour
-    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+    ["core.concealer"] = {
+      config = {
+        icons = {
+          todo = {
+            undone = {
+              icon = " ",
+            },
+          },
+        },
+      },
+    }, -- Adds pretty icons to your documents
     ["core.dirman"] = { -- Manages Neorg workspaces
       config = {
         workspaces = {
