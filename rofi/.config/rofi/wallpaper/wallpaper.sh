047d843af6f6cea6157d22a8b58d4ffb451b0b44
Shelby Tucker <shelby.tucker@gmail.com>
2024-10-12 11:49:56 -0400

    feat(rofi): wallpaper switching changes gtk theme colors

--- rofi/.config/rofi/wallpaper/wallpaper.sh
+++ rofi/.config/rofi/wallpaper/wallpaper.sh
@@ -22,9 +22,15 @@ wal --cols16 lighten -i ~/Pictures/wallpapers/"$P"
 cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors.css
 cp ~/.cache/wal/colors-rofi-dark.rasi ~/.config/rofi/shared/colors.rasi
 cp ~/.cache/wal/yazi-theme.toml ~/.config/yazi/theme.toml
+cp ~/.cache/wal/colors.Xresources ~/.Xresources
 
 pkill waybar
 hyprctl dispatch exec "waybar"
 
 pkill swaybg
 hyprctl dispatch exec "swaybg -i ~/Pictures/wallpapers/$P"
+
+xrdb -merge ~/.Xresources
+oomox-cli /opt/oomox/scripted_colors/xresources/xresources-reverse
+
+/opt/oomox/plugins/icons_gnomecolors/gnome-colors-icon-theme/change_color.sh -o oomox-xresources-reverse /opt/oomox/scripted_colors/xresources/xresources-reverse
