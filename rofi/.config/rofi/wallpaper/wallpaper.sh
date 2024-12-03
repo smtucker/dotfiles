1b14d01a80b4887de208d2875cbb29199aabbc21
Shelby Tucker <shelby.tucker@gmail.com>
2024-12-03 07:27:28 -0500

    feat(rofi): generate qt and gtk4 themes on wallpaper change

--- rofi/.config/rofi/wallpaper/wallpaper.sh
+++ rofi/.config/rofi/wallpaper/wallpaper.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+## Configuration
+THEMENAME="oomox-xresources-reverse"
+BASE16CLI="/opt/oomox/plugins/base16/cli.py"
+OOMOXPLUGINDIR="/opt/oomox/plugins/"
+QT_STYLE_TEMPLATE="$OOMOXPLUGINDIR/base16/templates/qt-oomox-styleplugin/templates/default.mustache"
+GTK4_TEMPLATE="$OOMOXPLUGINDIR/base16/templates/gtk4-oodwaita/templates/gtk4-libadwaita1.6.0.mustache"
+TEMPLATE="$OOMOXPLUGINDIR/import_xresources/colors/xresources-reverse"
+# TEMPLATE=/opt/oomox/plugins/base16/schemes/pywal/pywal.yml
+QT_STYLE_THEMES="$HOME/.config/oomox-qtstyleplugin/themes"
+OOMOXCOLOR=/opt/oomox/scripted_colors/xresources/xresources-reverse
 dir="$HOME/.config/rofi"
 theme='style-1'
 
-
-## Select wallpaper
+## Select wallpaper with rofi
 cd ~/Pictures/wallpapers || return 1
 P=$(for a in *; do echo -en "$a\0icon\x1f$a\n"; done |
 rofi \
@@ -16,38 +25,57 @@ if [ -z "$P" ]; then
   exit
 fi
 
-THEMENAME="oomox-xresources-reverse"
+NID=$(notify-send -p "Changing wallpaper" "Generating colors...")
 
-notify-send "Changing wallpaper" "Changing to $P"
+GENERATE_COLORS() {
+  wal --cols16 lighten -i ~/Pictures/wallpapers/"$P"
 
-wal --cols16 lighten -i ~/Pictures/wallpapers/"$P"
+  cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors.css
+  cp ~/.cache/wal/colors-rofi-dark.rasi ~/.config/rofi/shared/colors.rasi
+  cp ~/.cache/wal/yazi-theme.toml ~/.config/yazi/theme.toml
+  cp ~/.cache/wal/colors.Xresources ~/.Xresources
+}
 
-cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors.css
-cp ~/.cache/wal/colors-rofi-dark.rasi ~/.config/rofi/shared/colors.rasi
-cp ~/.cache/wal/yazi-theme.toml ~/.config/yazi/theme.toml
-cp ~/.cache/wal/colors.Xresources ~/.Xresources
+RELOAD_APPS(){
+  notify-send -r "$NID" "Changing Wallpaper" "Reloading apps..."
 
-pkill waybar
-hyprctl dispatch exec "waybar"
+  pkill waybar
+  hyprctl dispatch exec "waybar"
 
-pkill swaybg
-hyprctl dispatch exec "swaybg -i ~/Pictures/wallpapers/$P"
+  pkill swaybg
+  hyprctl dispatch exec "swaybg -i ~/Pictures/wallpapers/$P"
+}
 
-xrdb -merge ~/.Xresources
-oomox-cli /opt/oomox/scripted_colors/xresources/xresources-reverse
+GENERATE_GTK_THEMES(){
+  notify-send -r "$NID" "Changing Wallpaper" "Generating Gtk2/3 themes..."
+  xrdb -merge ~/.Xresources
+  oomox-cli /opt/oomox/scripted_colors/xresources/xresources-reverse
+  notify-send -r "$NID" "Changing Wallpaper" "Generating Gtk4 theme..."
+  python "${BASE16CLI}" "${GTK4_TEMPLATE}" "${TEMPLATE}" > "$HOME/.themes/$THEMENAME/gtk-4.0/gtk.css"
+  sed -i '/^Import Colors/d' "$HOME/.themes/$THEMENAME/gtk-4.0/gtk.css"
+  cp "$HOME/.themes/$THEMENAME/gtk-4.0/gtk.css" "$HOME/.config/gtk-4.0/gtk.css"
+}
 
-BASE16CLI="/opt/oomox/plugins/base16/cli.py"
-QT_STYLE_TEMPLATE="/opt/oomox/plugins/base16/templates/qt-oomox-styleplugin/templates/default.mustache"
-# TEMPLATE="/opt/oomox/plugins/import_xresources/colors/xresources-reverse"
-TEMPLATE=/opt/oomox/plugins/base16/schemes/pywal/pywal.yml
-QT_STYLE_THEMES="$HOME/.config/oomox-qtstyleplugin/themes"
+GENERATE_QT_STYLE() {
+  notify-send -r "$NID" "Changing Wallpaper" "Generating Qt themes..."
+  if [ -d "${QT_STYLE_THEMES}" ]; then
+    cp ~/.cache/wal/oomox-base16.yml "$TEMPLATE"
+    python "${BASE16CLI}" "${QT_STYLE_TEMPLATE}" "${TEMPLATE}" > "${QT_STYLE_THEMES}"/oomox.css
+    sed -i '/^Import Colors/d' "${QT_STYLE_THEMES}"/oomox.css
+  else
+    notify-send --icon=error --urgency=critical "Error outputing Qt Stylesheet" "${QT_STYLE_THEMES} does not exist"
+  fi
+}
 
-if [ -d "${QT_STYLE_THEMES}" ]; then
-  cp ~/.cache/wal/oomox-base16.yml $TEMPLATE
-  python ${BASE16CLI} ${QT_STYLE_TEMPLATE} ${TEMPLATE} > "${QT_STYLE_THEMES}"/oomox.css
-  sed -i '/^Import Colors/d' "${QT_STYLE_THEMES}"/oomox.css
-else
-  notify-send --icon=error --urgency=critical "Error outputing Qt Stylesheet" "${QT_STYLE_THEMES} does not exist"
-fi
+GENERATE_ICONS() {
+  notify-send -r "$NID" "Changing Wallpaper" "Generating icon theme..."
+  /opt/oomox/plugins/icons_gnomecolors/gnome-colors-icon-theme/change_color.sh -o "${THEMENAME}" "${OOMOXCOLOR}"
+}
 
-/opt/oomox/plugins/icons_gnomecolors/gnome-colors-icon-theme/change_color.sh -o ${THEMENAME} /opt/oomox/scripted_colors/xresources/xresources-reverse
+GENERATE_COLORS
+RELOAD_APPS
+GENERATE_GTK_THEMES
+GENERATE_QT_STYLE
+GENERATE_ICONS
+
+notify-send -r "$NID" "Wallpaper Changed" "Finishing switching wallpaper to $P"
