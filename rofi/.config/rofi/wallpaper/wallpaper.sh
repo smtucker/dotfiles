f9db5f62abe6ab6012856e47d52f394f54aa10e5
Shelby Tucker <shelby.tucker@gmail.com>
2024-12-10 14:04:02 -0500

    rofi(feat): add notifications and options to wallpaper switching

--- rofi/.config/rofi/wallpaper/wallpaper.sh
+++ rofi/.config/rofi/wallpaper/wallpaper.sh
@@ -25,7 +25,16 @@ if [ -z "$P" ]; then
   exit
 fi
 
-NID=$(notify-send -p "Changing wallpaper" "Generating colors...")
+NID=$(notify-send -p "Changing wallpaper" "Generating colors..." -h int:value:5 -a "Wallpaper Switcher")
+
+PRINT_USAGE() {
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  -r  Skip reloading applications"
+  echo "  -g  Skip generating GTK themes"
+  echo "  -q  Skip generating Qt styles"
+  echo "  -i  Skip generating icon theme"
+}
 
 GENERATE_COLORS() {
   wal --cols16 lighten -i ~/Pictures/wallpapers/"$P"
@@ -37,7 +46,9 @@ GENERATE_COLORS() {
 }
 
 RELOAD_APPS(){
-  notify-send -r "$NID" "Changing Wallpaper" "Reloading apps..."
+  notify-send -r "$NID" "Changing Wallpaper" "Reloading apps..." -h int:value:10 -a "Wallpaper Switcher"
+
+  swaync-client -rs
 
   pkill waybar
   hyprctl dispatch exec "waybar"
@@ -47,17 +58,17 @@ RELOAD_APPS(){
 }
 
 GENERATE_GTK_THEMES(){
-  notify-send -r "$NID" "Changing Wallpaper" "Generating Gtk2/3 themes..."
+  notify-send -r "$NID" "Changing Wallpaper" "Generating Gtk2/3 themes..." -h int:value:15 -a "Wallpaper Switcher"
   xrdb -merge ~/.Xresources
   oomox-cli /opt/oomox/scripted_colors/xresources/xresources-reverse
-  notify-send -r "$NID" "Changing Wallpaper" "Generating Gtk4 theme..."
+  notify-send -r "$NID" "Changing Wallpaper" "Generating Gtk4 theme..." -h int:value:35 -a "Wallpaper Switcher"
   python "${BASE16CLI}" "${GTK4_TEMPLATE}" "${TEMPLATE}" > "$HOME/.themes/$THEMENAME/gtk-4.0/gtk.css"
   sed -i '/^Import Colors/d' "$HOME/.themes/$THEMENAME/gtk-4.0/gtk.css"
   cp "$HOME/.themes/$THEMENAME/gtk-4.0/gtk.css" "$HOME/.config/gtk-4.0/gtk.css"
 }
 
 GENERATE_QT_STYLE() {
-  notify-send -r "$NID" "Changing Wallpaper" "Generating Qt themes..."
+  notify-send -r "$NID" "Changing Wallpaper" "Generating Qt themes..." -h int:value:55 -a "Wallpaper Switcher"
   if [ -d "${QT_STYLE_THEMES}" ]; then
     cp ~/.cache/wal/oomox-base16.yml "$TEMPLATE"
     python "${BASE16CLI}" "${QT_STYLE_TEMPLATE}" "${TEMPLATE}" > "${QT_STYLE_THEMES}"/oomox.css
@@ -68,14 +79,35 @@ GENERATE_QT_STYLE() {
 }
 
 GENERATE_ICONS() {
-  notify-send -r "$NID" "Changing Wallpaper" "Generating icon theme..."
+  notify-send -r "$NID" "Changing Wallpaper" "Generating icon theme..." -h int:value:75 -a "Wallpaper Switcher"
   /opt/oomox/plugins/icons_gnomecolors/gnome-colors-icon-theme/change_color.sh -o "${THEMENAME}" "${OOMOXCOLOR}"
 }
 
-GENERATE_COLORS
-RELOAD_APPS
-GENERATE_GTK_THEMES
-GENERATE_QT_STYLE
-GENERATE_ICONS
+while getopts "rgqi" opt; do
+  case $opt in
+    r)
+      SKIP_RELOAD=1
+      ;;
+    g)
+      SKIP_GTK=1
+      ;;
+    q)
+      SKIP_QT=1
+      ;;
+    i)
+      SKIP_ICONS=1
+      ;;
+    *)
+      PRINT_USAGE
+      notify-send -r "$NID" "Error" "Invalid option: $opt" -a "Wallpaper Switcher" -w -i error
+      ;;
+  esac
+done
 
-notify-send -r "$NID" "Wallpaper Changed" "Finishing switching wallpaper to $P"
+GENERATE_COLORS
+[ -n "$SKIP_RELOAD" ] || RELOAD_APPS
+[ -n "$SKIP_GTK" ] || GENERATE_GTK_THEMES
+[ -n "$SKIP_QT" ] || GENERATE_QT_STYLE
+[ -n "$SKIP_ICONS" ] || GENERATE_ICONS
+
+notify-send -r "$NID" "Wallpaper Changed" "Finishing switching wallpaper to $P" -a "Wallpaper Switcher"
