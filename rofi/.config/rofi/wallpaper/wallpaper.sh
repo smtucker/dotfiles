d790c982653676ca0b416df74f36ef2482eb1eb4
Shelby Tucker <shelby.tucker@gmail.com>
2024-12-16 13:50:02 -0500

    feat(rofi): major refactor of the wallpaper switching script

--- rofi/.config/rofi/wallpaper/wallpaper.sh
+++ rofi/.config/rofi/wallpaper/wallpaper.sh
@@ -1,57 +1,87 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Configuration
+declare -A CONFIG=(
+    ["THEMENAME"]="oomox-xresources-reverse"
+    ["BASE16CLI"]="/opt/oomox/plugins/base16/cli.py"
+    ["OOMOXPLUGINDIR"]="/opt/oomox/plugins/"
+    ["QT_STYLE_TEMPLATE"]="${OOMOXPLUGINDIR}/base16/templates/qt-oomox-styleplugin/templates/default.mustache"
+    ["GTK4_TEMPLATE"]="${OOMOXPLUGINDIR}/base16/templates/gtk4-oodwaita/templates/gtk4-libadwaita1.6.0.mustache"
+    ["TEMPLATE"]="$OOMOXPLUGINDIR/import_xresources/colors/xresources" # Default template, no reverse
+    ["QT_STYLE_THEMES"]="$HOME/.config/oomox-qtstyleplugin/themes"
+    ["OOMOXCOLOR"]="/opt/oomox/scripted_colors/xresources/xresources" # Default color, no reverse
+    ["WALLPAPER_DIR"]="$HOME/Pictures/wallpapers" # Wallpaper directory
+)
+
+
+# --- Functions ---
 
 function CRIT_ERROR() {
-  echo "ERROR: $1"
-  if [ -n "$NID" ]; then
-    notify-send -a "Wallpaper Switcher" "ERROR: $1" --urgency=critical
-  else
-    notify-send -a "Wallpaper Switcher" -r "$NID" "ERROR: $1" --urgency=critical
-  fi
-  return 1
+    local message="$1"
+    echo "ERROR: ${message}"
+    NOTIFY "${message}" critical
+    return 1
 }
 
-## Pick light or dark mode
-MODE=$(printf "Light\nDark\n" |
-  rofi -dmenu \
-    -theme "${SCRIPT_DIR}"/mode-select.rasi )
 
-case $MODE in
-  Light)
-    LIGHTMODE=lighten
-    REVERSE=""
-    ;;
-  Dark)
-    LIGHTMODE=darken
-    REVERSE="-reverse"
-    ;;
-  *) CRIT_ERROR "Invalid mode" ; exit ;;
-esac
+function NOTIFY() {
+    local message="$1"
+    local progress="$2"
+    local urgency="$3"
+    local options=""
 
-## Select wallpaper with rofi
-cd ~/Pictures/wallpapers || CRIT_ERROR "Cannot find wallpaper directory"
-P=$(for a in *; do echo -en "$a\0icon\x1f$a\n"; done |
-rofi \
-    -dmenu \
-    -theme "${SCRIPT_DIR}"/wallpaper-select.rasi)
+    if [ -n "${progress}" ]; then
+        options+="-h int:value:${progress} "
+    fi
 
-## Quit if P is empty
-if [ -z "$P" ]; then 
-  exit
-fi
+    if [ -n "${urgency}" ]; then
+        options+="--urgency=${urgency} "
+    fi
 
-## Configuration
-THEMENAME="oomox-xresources-reverse"
-BASE16CLI="/opt/oomox/plugins/base16/cli.py"
-OOMOXPLUGINDIR="/opt/oomox/plugins/"
-QT_STYLE_TEMPLATE="$OOMOXPLUGINDIR/base16/templates/qt-oomox-styleplugin/templates/default.mustache"
-GTK4_TEMPLATE="$OOMOXPLUGINDIR/base16/templates/gtk4-oodwaita/templates/gtk4-libadwaita1.6.0.mustache"
-TEMPLATE="$OOMOXPLUGINDIR/import_xresources/colors/xresources$REVERSE"
-# TEMPLATE=/opt/oomox/plugins/base16/schemes/pywal/pywal.yml
-QT_STYLE_THEMES="$HOME/.config/oomox-qtstyleplugin/themes"
-OOMOXCOLOR="/opt/oomox/scripted_colors/xresources/xresources$REVERSE"
-NID=$(notify-send -p "Changing wallpaper" "Generating colors..." -h int:value:5 -a "Wallpaper Switcher")
+    if [ -n "$NID" ]; then
+        notify-send -a "Wallpaper Switcher" -r "$NID" "${message}" ${options}
+    else
+        NID=$(notify-send -p -a "Wallpaper Switcher" "${message}" ${options} -h int:value:0)
+    fi
+}
+
+
+function SET_MODE() {
+    case "$1" in
+        Light)
+            LIGHTMODE=lighten
+            ;;
+        Dark)
+            LIGHTMODE=darken
+            REVERSE="-reverse"
+            ;;
+        *) CRIT_ERROR "Invalid mode" ; exit ;;
+    esac
+
+    # Update config template and color based on mode.
+    CONFIG["TEMPLATE"]="${CONFIG["OOMOXPLUGINDIR"]}/import_xresources/colors/xresources${REVERSE}"
+    CONFIG["OOMOXCOLOR"]="/opt/oomox/scripted_colors/xresources/xresources${REVERSE}"
+}
+
+
+function SELECT_WALLPAPER() {
+    cd "${CONFIG[WALLPAPER_DIR]}" || CRIT_ERROR "Cannot find wallpaper directory"
+
+    local selection
+    selection=$(for a in *; do echo -en "$a\0icon\x1f$a\n"; done |
+        rofi \
+            -dmenu \
+            -theme "${SCRIPT_DIR}"/wallpaper-select.rasi)
+
+    # Quit if selection is empty.
+    if [ -z "$selection" ]; then
+        exit
+    fi
+    echo "$selection"
+}
+
 
 function PRINT_USAGE() {
   echo "Usage: $0 [options]"
@@ -62,52 +92,66 @@ function PRINT_USAGE() {
   echo "  -i  Skip generating icon theme"
 }
 
+
 function GENERATE_COLORS() {
-  wal --cols16 lighten -i ~/Pictures/wallpapers/"$P"
+    NOTIFY "Generating colors..." 5
+    wal --cols16 "$LIGHTMODE" -i "${CONFIG[WALLPAPER_DIR]}/$P"
 
-  cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors.css
-  cp ~/.cache/wal/colors-rofi-dark.rasi ~/.config/rofi/shared/colors.rasi
-  cp ~/.cache/wal/yazi-theme.toml ~/.config/yazi/theme.toml
-  cp ~/.cache/wal/colors.Xresources ~/.Xresources
+    cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors.css
+    cp ~/.cache/wal/colors-rofi-dark.rasi ~/.config/rofi/shared/colors.rasi
+    cp ~/.cache/wal/yazi-theme.toml ~/.config/yazi/theme.toml
+    cp ~/.cache/wal/colors.Xresources ~/.Xresources
 }
 
-function RELOAD_APPS(){
-  notify-send -r "$NID" "Changing Wallpaper" "Reloading apps..." -h int:value:10 -a "Wallpaper Switcher"
 
-  swaync-client -rs
+function RELOAD_APPS() {
+    NOTIFY "Reloading apps..." 10
+    swaync-client -rs
 
-  pkill waybar
-  hyprctl dispatch exec "waybar"
+    pkill waybar
+    hyprctl dispatch exec "waybar"
 
-  pkill swaybg
-  hyprctl dispatch exec "swaybg -i ~/Pictures/wallpapers/$P"
+    pkill swaybg
+    hyprctl dispatch exec "swaybg -i ${CONFIG[WALLPAPER_DIR]}/$P"
 }
 
-function GENERATE_GTK_THEMES(){
-  notify-send -r "$NID" "Changing Wallpaper" "Generating Gtk2/3 themes..." -h int:value:15 -a "Wallpaper Switcher"
-  xrdb -merge ~/.Xresources
-  oomox-cli /opt/oomox/scripted_colors/xresources/xresources${REVERSE} -o oomox-xresources-reverse
-  notify-send -r "$NID" "Changing Wallpaper" "Generating Gtk4 theme..." -h int:value:35 -a "Wallpaper Switcher"
-  python "${BASE16CLI}" "${GTK4_TEMPLATE}" "${TEMPLATE}" > "$HOME/.themes/$THEMENAME/gtk-4.0/gtk.css"
-  sed -i '/^Import Colors/d' "$HOME/.themes/$THEMENAME/gtk-4.0/gtk.css"
-  cp "$HOME/.themes/$THEMENAME/gtk-4.0/gtk.css" "$HOME/.config/gtk-4.0/gtk.css"
+
+function GENERATE_GTK_THEMES() {
+    NOTIFY "Generating Gtk2/3 themes..." 15
+    xrdb -merge ~/.Xresources
+    oomox-cli "${CONFIG[OOMOXCOLOR]}" -o "${CONFIG[THEMENAME]}"
+
+    NOTIFY "Generating Gtk4 theme..." 35
+    python "${CONFIG[BASE16CLI]}" "${CONFIG[GTK4_TEMPLATE]}" "${CONFIG[TEMPLATE]}" > "$HOME/.themes/${CONFIG[THEMENAME]}/gtk-4.0/gtk.css"
+    sed -i '/^Import Colors/d' "$HOME/.themes/${CONFIG[THEMENAME]}/gtk-4.0/gtk.css"
+    cp "$HOME/.themes/${CONFIG[THEMENAME]}/gtk-4.0/gtk.css" "$HOME/.config/gtk-4.0/gtk.css"
 }
+
 
 function GENERATE_QT_STYLE() {
-  notify-send -r "$NID" "Changing Wallpaper" "Generating Qt themes..." -h int:value:55 -a "Wallpaper Switcher"
-  if [ -d "${QT_STYLE_THEMES}" ]; then
-    cp ~/.cache/wal/oomox-base16.yml "$TEMPLATE"
-    python "${BASE16CLI}" "${QT_STYLE_TEMPLATE}" "${TEMPLATE}" > "${QT_STYLE_THEMES}"/oomox.css
-    sed -i '/^Import Colors/d' "${QT_STYLE_THEMES}"/oomox.css
-  else
-    notify-send --icon=error --urgency=critical "Error outputing Qt Stylesheet" "${QT_STYLE_THEMES} does not exist"
-  fi
+    NOTIFY "Generating Qt themes..." 55
+    if [ -d "${CONFIG[QT_STYLE_THEMES]}" ]; then
+        cp ~/.cache/wal/oomox-base16.yml "${CONFIG[TEMPLATE]}"
+        python "${CONFIG[BASE16CLI]}" "${CONFIG[QT_STYLE_TEMPLATE]}" "${CONFIG[TEMPLATE]}" > "${CONFIG[QT_STYLE_THEMES]}/oomox.css"
+        sed -i '/^Import Colors/d' "${CONFIG[QT_STYLE_THEMES]}/oomox.css"
+    else
+        notify-send --icon=error --urgency=critical "Error outputing Qt Stylesheet" "${CONFIG[QT_STYLE_THEMES]} does not exist"
+    fi
 }
 
+
 function GENERATE_ICONS() {
-  notify-send -r "$NID" "Changing Wallpaper" "Generating icon theme..." -h int:value:75 -a "Wallpaper Switcher"
-  /opt/oomox/plugins/icons_gnomecolors/gnome-colors-icon-theme/change_color.sh -o "${THEMENAME}" "${OOMOXCOLOR}"
+    NOTIFY "Generating icon theme..." 75
+    /opt/oomox/plugins/icons_gnomecolors/gnome-colors-icon-theme/change_color.sh -o "${CONFIG[THEMENAME]}" "${CONFIG[OOMOXCOLOR]}"
 }
+
+
+# --- Main execution ---
+
+MODE=$(printf "Light\nDark\n" | rofi -dmenu -theme "${SCRIPT_DIR}"/mode-select.rasi)
+SET_MODE "$MODE"
+
+P=$(SELECT_WALLPAPER)
 
 while getopts "rgqi" opt; do
   case $opt in
@@ -130,10 +174,11 @@ while getopts "rgqi" opt; do
   esac
 done
 
+
 GENERATE_COLORS
 [ -n "$SKIP_RELOAD" ] || RELOAD_APPS
 [ -n "$SKIP_GTK" ] || GENERATE_GTK_THEMES
 [ -n "$SKIP_QT" ] || GENERATE_QT_STYLE
 [ -n "$SKIP_ICONS" ] || GENERATE_ICONS
 
-notify-send -r "$NID" "Wallpaper Changed" "Finishing switching wallpaper to $P" -a "Wallpaper Switcher"
+NOTIFY "Wallpaper Changed to $P"
